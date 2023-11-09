b351c32aba117e9b74cc711c678f37ac79d9798d
eskenderov <eskenderovar@yandex.ru>
2023-11-09 15:28:24 +0300

    feat: update home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,24 @@
+import Image from "next/image";
 import s from "./page.module.scss";
 export default function Home() {
   return (
-    <main className="page">
-      <div className="container">Home</div>
+    <main className="page" style={{ margin: 0 }}>
+      <div className={s.hero}>
+        <Image
+          alt="Toyota gt86"
+          src="/images/skaly.jpeg"
+          placeholder="blur"
+          blurDataURL="/images/skaly.jpeg"
+          quality={100}
+          fill
+          priority
+          sizes="100vw"
+          style={{
+            objectFit: "cover",
+          }}
+        />
+      </div>
+      <div className="container"></div>
     </main>
   );
 }
